eb6d896fb4fe530acce1f57c6e0e1a32fac25834
Jonas <jonas.fo@live.se>
2019-11-14 14:53:09 +0100

    Implemented way of calculating hand. Betting for reflex agent. Split code into phases.

--- Lab1/Task2/agent.py
+++ Lab1/Task2/agent.py
@@ -1,5 +1,6 @@
 import random
 
+
 class Agent:
     def __init__(self, agentType):
         self.agentType = agentType
@@ -8,10 +9,34 @@ class Agent:
         self.currentBet = 0
 
     def bet(self):
-        if self.agentType == 'random':
-            bet = random.randrange(0,50,1)      # Random bet between $0-50.
+        if self.agentType == 'random':              # Random agent betting.
+            bet = random.randrange(0, 50, 1)        # Random bet between $0-50.
             print(f'{self.agentType} agent bet ${bet}.')
             return bet
-        elif self.agent.agentType == 'reflex':
-
+        elif self.agentType == 'fixed':      # Fixed agent betting.
+            hand = self.calculateHand()
+            if hand == 'High cards!':
+                bet = 15
+            elif hand == 'Two of a kind!':
+                bet = 25
+            elif hand == 'Three of a kind!':
+                bet = 50
             print(f'{self.agentType} agent bet ${bet}.')
+            return bet
+
+    def revealHand(self):
+        print(f'Hand of {self.agentType} agent: {self.hand}.')
+
+    # Possible hands are "three of a kind", "a pair" and "high cards".
+    def calculateHand(self):
+        ranks = list()
+        for card in self.hand:
+            for rank in card:
+                ranks.append(rank)
+
+        if ranks[0] == ranks[2] == ranks[4]:
+            return 'Three of a kind!'
+        elif ranks[0] == ranks[2] or ranks[0] == ranks[4] or ranks[2] == ranks[4]:
+            return 'Two of a kind!'
+        else:
+            return 'High cards!'
--- Lab1/Task2/main.py
+++ Lab1/Task2/main.py
@@ -6,34 +6,89 @@ totalPot = 0
 # Rank: {2, 3, 4, 5, 6, 7, 8, 9, T, J, Q, K, A}
 # Suit: {s, h, d, c}
 deck = list()
+randomAgent = agent.Agent('random')
+fixedAgent = agent.Agent('fixed')
+
+'''ranks = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
+suits = ['s', 'h', 'd', 'c']'''
 
 
 def generateDeck():
     global deck
     ranks = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
     suits = ['s', 'h', 'd', 'c']
-    index = 0
+
     for cardSuit in suits:
-        print(cardSuit)
         for cardRank in ranks:
             deck.append(cardRank+cardSuit)
 
-    random.shuffle(deck)
+    random.shuffle(deck)    # Shuffles deck.
 
 
 def assignHand(agent):
-    #ranks.pop(random.randrange(0, 12))
-    print(f'Hand dealt to {agent.agentType} agent')
+    global deck
+    for cardNumber in range(0, 3):      # Hand three cards to the agent.
+        agent.hand.append(deck.pop())
+    agent.hand.sort()
+    print(f'Hand dealt to {agent.agentType} agent.')
 
 
-# Main.
-if __name__ == '__main__':
-    randomAgent = agent.Agent('random')
-    totalPot += randomAgent.bet()
-    print(f'Total pot: ${totalPot}')
+def compareHands(firstAgent, secondAgent):
+    global totalPot
+    if firstAgent.calculateHand() and secondAgent.calculateHand() == 'High cards!':
+        handTuple = tuple(zip(firstAgent.hand, secondAgent.hand))
+        for index in handTuple:
+            if index[0] > index[1]:
+                assignWin(firstAgent)
+                break
+            else:
+                assignWin(secondAgent)
+                break
+
+
+def assignWin(agent):
+    global totalPot
+    print(f'{agent.agentType} agent wins!')
+    agent.moneyWon = totalPot
+    totalPot = 0
+    print()
+
+
+def biddingPhase():
+    global totalPot
+    for x in range(1, 4):
+        print(f'Bidding phase {x}:')
+        totalPot += randomAgent.bet()
+        totalPot += fixedAgent.bet()
+        print()
+
+
+def cardDealingPhase():
+    print('Card dealing phase:')
     assignHand(randomAgent)
-    #testHand = ranks[0] + suits[0]
-    # print(testHand)
+    assignHand(fixedAgent)
+    randomAgent.revealHand()
+    fixedAgent.revealHand()
+
+
+    # Main.
+if __name__ == '__main__':
     generateDeck()
-    for card in deck:
-        print(card)
+    cardDealingPhase()
+    print()
+
+    '''assignHand(randomAgent)
+    assignHand(fixedAgent)
+    totalPot += randomAgent.bet()
+    totalPot += fixedAgent.bet()
+    randomAgent.revealHand()
+    fixedAgent.revealHand()'''
+    biddingPhase()
+    print()
+
+    randomAgent.calculateHand()
+    fixedAgent.calculateHand()
+    print(f'Total pot: ${totalPot}')
+    compareHands(randomAgent, fixedAgent)
+    print(f'{randomAgent.agentType} agent has won ${randomAgent.moneyWon}.')
+    print(f'{fixedAgent.agentType} agent has won ${fixedAgent.moneyWon}.')
