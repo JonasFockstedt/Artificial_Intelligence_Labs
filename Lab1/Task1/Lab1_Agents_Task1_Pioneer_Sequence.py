210ab4c26ae46d40bbeb561313e073e0e4f56ca2
Jonas <jonas.fo@live.se>
2019-11-15 14:27:53 +0100

    Adjusted sequence agent. Now only uses World.execute()

--- Lab1/Task1/Lab1_Agents_Task1_Pioneer_Sequence.py
+++ Lab1/Task1/Lab1_Agents_Task1_Pioneer_Sequence.py
@@ -29,42 +29,9 @@ print('Time:', simulationTime,
 # Reasoning: figure out which action to take #
 ##############################################
 
-motorSpeed = dict(speedLeft=2, speedRight=2)    # Straight forward.
-World.setMotorSpeeds(motorSpeed)
-for x in range(0, 75000000):                    # Wait.
-    pass
-
-motorSpeed = dict(speedLeft=0, speedRight=0)    # Stop.
-World.setMotorSpeeds(motorSpeed)
-World.collectNearestBlock()                     # Collect first energy block.
-
-motorSpeed = dict(speedLeft=0, speedRight=2)    # Turn right.
-World.setMotorSpeeds(motorSpeed)
-for x in range(0, 30000000):                    # Wait.
-    pass
-
-motorSpeed = dict(speedLeft=2, speedRight=2)    # Straight forward.
-World.setMotorSpeeds(motorSpeed)
-for x in range(0, 95000000):                    # Wait.
-    pass
-
+World.execute(dict(speedLeft=2, speedRight=2), 6000, -1)    # Go forward.
 World.collectNearestBlock()
-'''elif World.getSimulationTime() < 10000:
-    motorSpeed = dict(speedLeft=-1.5, speedRight=-1.0)
-    World.setMotorSpeeds(motorSpeed)
-elif World.getSimulationTime() < 15000:
-    print("Turning for a bit...",)
-    World.execute(dict(speedLeft=2, speedRight=-2), 15000, -1)
-    print("... got dizzy, stopping!")
-    print("BTW, nearest energy block is at:",
-          World.getSensorReading("energySensor"))
-    World.setMotorSpeeds(motorSpeed)'''
-
-########################################
-# Action Phase: Assign speed to wheels #
-########################################
-# assign speed to the wheels
-World.setMotorSpeeds(motorSpeed)
-# try to collect energy block (will fail if not within range)
-
-print("Trying to collect a block...", World.collectNearestBlock())
+World.execute(dict(speedLeft=0, speedRight=2), 2900, -1)    # Turn left.
+World.collectNearestBlock()
+World.execute(dict(speedLeft=2, speedRight=2), 8500, -1)    # Go forward.
+World.collectNearestBlock()
