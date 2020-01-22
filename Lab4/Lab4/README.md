0f887a3889f0ca8143c75c2c8ecfe52894643f7f
Jonas <jonas.fo@live.se>
2020-01-22 20:28:33 +0100

    Second commit.

---
+++ Lab4/Lab4/README.md
@@ -0,0 +1,19 @@
+# Task 1: Vechicle fleet management
+
+## Part A: k-NN classifier
+
+## Part B: Investigate different classification algorithms
+
+## Part C: Parameter tuning
+
+## Part D: extra credit
+
+## Part E: Implement k-NN regression
+
+## Part F: Investigate different regression algorithms
+
+## Part G: Parameter tuning
+
+## Part H: extra credit
+
+# Task 2: Poker Project (machine learning)
--- Lab4/Lab4/Task 1/Example.py
+++ Lab4/Lab4/Task 1/Example.py
@@ -1,10 +1,11 @@
 import numpy as np
-from sklearn.cross_validation import train_test_split
+#from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn import svm
 
 # import data
 Data = np.loadtxt(open("Lab4Data.csv", "rb"), delimiter=";", skiprows=1)
-print ('*******************************************')
+print('*******************************************')
 print('Length of Total Data:', len(Data))
 
 Train_set, Test_set = train_test_split(Data, test_size=0.2)
@@ -13,20 +14,22 @@ Target_train = Train_set[:, 9]  # output labels
 
 Input_test = Test_set[:, :8]
 Target_test = Test_set[:, 9]
-print ('*******************************************')
+print('*******************************************')
 print('Length of Train Data:', len(Train_set))
 print('Length of Test Data', len(Test_set))
 
 
 svc = svm.SVC(kernel='linear', C=1.0).fit(Input_train, Target_train)
 #svc = svm.SVC(kernel='rbf', degree=3, C=1.0).fit(Input_train, Target_train)
-print ('*******************************************')
+print('*******************************************')
 print(svc)
 
 PredictedOutcome = svc.predict(Input_test)
-Number_of_Correct_Predictions = len([i for i, j in zip(PredictedOutcome, Target_test) if i == j])
+Number_of_Correct_Predictions = len(
+    [i for i, j in zip(PredictedOutcome, Target_test) if i == j])
 
-print ('*******************************************')
+print('*******************************************')
 print('Number of Correct Predictions:', Number_of_Correct_Predictions, 'Out_of:', len(PredictedOutcome),
       'Number of Test Data')
-print('Accuracy of Prediction in Percent:', (Number_of_Correct_Predictions/float(len(PredictedOutcome)))*100)
+print('Accuracy of Prediction in Percent:',
+      (Number_of_Correct_Predictions/float(len(PredictedOutcome)))*100)
