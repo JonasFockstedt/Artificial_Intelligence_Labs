7f57ae99c06058c68d8998466c5ebf12bedd979c
Jonas <jonas.fo@live.se>
2020-06-01 12:51:09 +0200

    Now extracts the player 2 final action from the original list.

--- Lab4/Lab4/Task 2/PartB.py
+++ Lab4/Lab4/Task 2/PartB.py
@@ -29,20 +29,34 @@ def removeEmptyString(the_list):
 
 
 # Determines the final action of player 2.
-def player2FinalAction(actions_list):
+def extractPlayer2FinalActions(actions_list):
     p2_final_actions = []
-    for action in actions_list:
+    for index, action in enumerate(actions_list):
         # Split the last action of both players.
         splitted_string = action[-1].split()
         # If the length of the separated string is greater than 2,
         # then the final action of p2 is at index 2.
         if len(splitted_string) > 2:
+            # Append p2 final action to list.
             p2_final_actions.append(splitted_string[2])
+
+            # Extract p2 final action from original list.
+            temp_string = splitted_string[0] + ' ' + splitted_string[1]
+            temp_list = action[:-1]
+            temp_list.append(temp_string)
+            actions_list[index] = temp_list
 
         # If the length of the separated string is lesser than or equal to 2,
         # then the last action of p2 is from the previous action round.
         elif len(splitted_string) <= 2:
+            # Append p2 final action to list.
             p2_final_actions.append(action[-2].split()[2])
+
+            # Extract what comes after p2 final action and replace in the original list.
+            temp_string = action[-2].split()[0] + ' ' + action[-2].split()[1]
+            temp_list = action[:-2]
+            temp_list.append(temp_string)
+            actions_list[index] = temp_list
 
     return p2_final_actions
 
@@ -55,8 +69,9 @@ if __name__ == '__main__':
     removeEmptyString(actions_list)
 
     # Extract final actions of p2 (target values).
-    p2_final_actions = player2FinalAction(actions_list)
+    p2_final_actions = extractPlayer2FinalActions(actions_list)
     print(p2_final_actions)
+    print(actions_list)
 
     # Split data.
     train_set, test_set = train_test_split(actions_list, test_size=0.2)
